fa3709a79b45b8febc318f402f74260c8acf7a4f
Raja R <raajarangasamy@gmail.com>
2026-06-19 16:32:02 +0530

    Update check_videos.py

--- ISL - Manual Features/src/check_videos.py
+++ ISL - Manual Features/src/check_videos.py
@@ -1,6 +1,7 @@
 import os
 
-video_folder = "../data"
+video_folder = r"C:\Users\revas\PycharmProjects\ISL convert into text\src\video_processor.py\data\raw_videos"
 
 for file in os.listdir(video_folder):
     print(file)
+
